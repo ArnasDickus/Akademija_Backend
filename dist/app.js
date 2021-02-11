5e4be9a49a3631fa624e515cba8770fe6646406e
Arnas Dickus <arnasdickus@protonmail.com>
2021-02-11 06:09:01 +0200

    installed babel and typescript

--- dist/app.js
+++ dist/app.js
@@ -1,31 +1,40 @@
-var createError = require('http-errors');
+"use strict";
+
+var _httpErrors = _interopRequireDefault(require("http-errors"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var express = require('express');
+
 var path = require('path');
+
 var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
+
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
+
 var testAPIRouter = require("./routes/testAPI");
 
-var app = express();
+var app = express(); // view engine setup
 
-// view engine setup
 app.set('view engine', 'jade');
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use("/testAPI", testAPIRouter); // catch 404 and forward to error handler
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  next((0, _httpErrors.default)(404));
 });
-
 module.exports = app;
---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import createError from 'http-errors';
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
+
+var app = express();
+
+// view engine setup
+app.set('view engine', 'jade');
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
+
+// catch 404 and forward to error handler
+app.use(function( req: any, res: any, next: any) {
+  next(createError(404));
+});
+
+module.exports = app;

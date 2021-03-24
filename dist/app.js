f9743627db1e499d63a4c827f5a4490bf56bf5cc
Arnas Dickus <arnasdickus@protonmail.com>
2021-03-24 07:57:42 +0200

    stashing chagnes

--- dist/app.js
+++ dist/app.js
@@ -1,45 +1,15 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const add = (a, b) => {
+    return a + b;
+};
+app.get('/', (req, res) => {
+    console.log(add(5, 5));
+    res.send('hello');
 });
-exports.default = void 0;
-
-var _httpErrors = _interopRequireDefault(require("http-errors"));
-
-var _express = _interopRequireDefault(require("express"));
-
-var _cookieParser = _interopRequireDefault(require("cookie-parser"));
-
-var _morgan = _interopRequireDefault(require("morgan"));
-
-var _cors = _interopRequireDefault(require("cors"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-const indexRouter = require('./routes/index');
-
-const usersRouter = require('./routes/users');
-
-const testAPIRouter = require("./routes/testAPI");
-
-const app = (0, _express.default)(); // view engine setup
-
-app.set('view engine', 'jade');
-app.use((0, _cors.default)());
-app.use((0, _morgan.default)('dev'));
-app.use(_express.default.json());
-app.use(_express.default.urlencoded({
-  extended: false
-}));
-app.use((0, _cookieParser.default)());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter); // catch 404 and forward to error handler
-
-app.use((req, res, next) => {
-  next((0, _httpErrors.default)(404));
-}); // module.exports = app;
-
-var _default = app;
-exports.default = _default;
+app.listen(5000, () => console.log('Server running'));
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,10 @@
-import createError from 'http-errors';
-import express, {Request, Response, NextFunction} from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import cors from 'cors';
+import express, {Application, Request, Response} from 'express';
 
-const indexRouter =  require('./routes/index');
-const usersRouter = require('./routes/users');
-const testAPIRouter = require("./routes/testAPI");
+const app: Application = express();
 
-const app = express();
 
-// view engine setup
-app.set('view engine', 'jade');
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
-
-// catch 404 and forward to error handler
-app.use(( req: Request, res: Response, next: NextFunction) => {
-  next(createError(404));
+app.get('/', (req: Request, res: Response) => {
+   res.send('hello'); 
 });
 
-// module.exports = app;
-export default app;
+app.listen(5000, () => console.log('Server running'))

b130cf601447756a8fea610cf30beeb698aad675
Arnas Dickus <arnasdickus@protonmail.com>
2021-03-25 06:37:41 +0200

    added typescript and connected mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, {Application, Request, Response} from 'express';
+import { connectDB } from './config/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
+
+connectDB();
 
 
 app.get('/', (req: Request, res: Response) => {
---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    let mongoUri: string = '';
+
+    if(process.env.MONGO_URI) {
+        mongoUri = process.env.MONGO_URI;
+    }
+    
+    try {
+        await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
